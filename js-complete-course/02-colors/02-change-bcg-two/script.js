e1a458700b9df49b77fc8282c934d43e2530c121
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-05 15:51:09 +0200

    Exercise colors 2.2 done

--- js-complete-course/02-colors/02-change-bcg-two/script.js
+++ js-complete-course/02-colors/02-change-bcg-two/script.js
@@ -9,8 +9,9 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+let runButton = document.getElementById("run")
 
-    // your code here
-
-})();
+runButton.addEventListener("click", function (){
+    let colorInput = document.getElementById("color")
+    document.body.style.backgroundColor = colorInput.value
+})
