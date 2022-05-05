87a94814e2189377f9ae2ce4621ca4dcb6798846
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-05 16:56:07 +0200

    Exercise 3.1 done

--- js-complete-course/03-dates/01-title-by-hour-one/script.js
+++ js-complete-course/03-dates/01-title-by-hour-one/script.js
@@ -11,8 +11,12 @@
 
 (function() {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
-
-    // your code here
+    const p = document.getElementById("target")
+    const myDate = new Date()
+    if (myDate.getHours() < 18) {
+        document.getElementById("target").innerHTML = "Hello"
+    } else {
+        document.getElementById("target").innerHTML = "Good Evening"
+    }
 
 })();
