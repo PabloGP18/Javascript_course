69589acc76e97eeebc5c89c6ec6869acf2b194e4
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-05 16:18:12 +0200

    Exercise 2.3 done

--- js-complete-course/02-colors/03-change-bcg-three/script.js
+++ js-complete-course/02-colors/03-change-bcg-three/script.js
@@ -9,8 +9,11 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+let runButton = document.getElementById("run")
 
-    // your code here
+ runButton.addEventListener ("click",function (){
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+});
 
-})();
+//setInterval to generate random colors without pushing the button every time
