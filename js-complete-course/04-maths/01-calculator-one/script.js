9f4a0419c9e60352d2deffceca4b2444c892f3fd
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-06 16:58:37 +0200

    Exercise 4.1 done

--- js-complete-course/04-maths/01-calculator-one/script.js
+++ js-complete-course/04-maths/01-calculator-one/script.js
@@ -9,22 +9,39 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-    // to get the value of an input: document.getElementById("element-id").value
 
-    document.getElementById("addition").addEventListener("click", function() {
-        // perform an addition
+(function calculator(){
+
+    document.getElementById("addition").addEventListener("click", function addition() {
+
+        const opOne = document.getElementById("op-one").value;
+        const opTwo = document.getElementById("op-two").value;
+
+        document.getElementById("target").innerHTML = parseInt(opOne) + parseInt(opTwo);
     });
 
-    document.getElementById("substraction").addEventListener("click", function() {
-        // perform an substraction
+    document.getElementById("substraction").addEventListener("click", function substraction() {
+
+        const opOne = document.getElementById("op-one").value;
+        const opTwo = document.getElementById("op-two").value;
+
+        document.getElementById("target").innerHTML = parseInt(opOne) - parseInt(opTwo);
+
     });
 
-    document.getElementById("multiplication").addEventListener("click", function() {
-        // perform an multiplication
+    document.getElementById("multiplication").addEventListener("click", function multiplication() {
+
+        const opOne = document.getElementById("op-one").value;
+        const opTwo = document.getElementById("op-two").value;
+
+        document.getElementById("target").innerHTML = parseInt(opOne) * parseInt(opTwo);
     });
 
-    document.getElementById("division").addEventListener("click", function() {
-        // perform an division
+    document.getElementById("division").addEventListener("click", function division() {
+
+        const opOne = document.getElementById("op-one").value;
+        const opTwo = document.getElementById("op-two").value;
+
+        document.getElementById("target").innerHTML = parseInt(opOne) / parseInt(opTwo);
     });
 })();
