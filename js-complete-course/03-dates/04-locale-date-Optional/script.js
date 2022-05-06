793f167c3d589eedcf53383068ca83bbacaa23f5
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-06 15:06:10 +0200

    Exercise 3.4

--- js-complete-course/03-dates/04-locale-date-Optional/script.js
+++ js-complete-course/03-dates/04-locale-date-Optional/script.js
@@ -9,10 +9,26 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+(function changingTextByDate () {
 
-    // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
+    const date = new Date();
+    /*
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    //const day = date.getDate() //;
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    //const seconds = date.getSeconds();//
 
-    // your code here
+
+    const monthsWritten = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const dayWritten = ["Monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"];
+
+    const time = (`${hour}h${minutes}`)
+
+    const dateWithSlashes = [monthsWritten[date.getMonth()], month + 1, dayWritten [date.getDay() - 1] ,year , time].join(' ');
+    */
+
+    document.getElementById("target").innerHTML = date;
 
 })();
