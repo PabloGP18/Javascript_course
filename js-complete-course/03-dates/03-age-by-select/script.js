afe3e4f71f95a335c301d3d55df41225ade277fd
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-06 14:07:32 +0200

    Exercise 3.3 done / Chapter 3 done

--- js-complete-course/03-dates/03-age-by-select/script.js
+++ js-complete-course/03-dates/03-age-by-select/script.js
@@ -9,8 +9,27 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
 
-    // your code here
+const button = document.getElementById('run').addEventListener('click', calcAge);
 
-})();
+function calcAge(){
+
+    let day = document.getElementById('dob-day').value;
+    let month = document.getElementById('dob-month').value;
+    let year = document.getElementById('dob-year').value;
+
+    let dateInput = new Date(year, month, day)
+
+    let dateNow = new Date();
+
+    let calcYear = dateNow.getFullYear() - dateInput.getFullYear();
+    let calcMonth = dateNow.getMonth() - dateInput.getMonth() +1;
+    let calcDay = dateNow.getDay() - dateInput.getDay() + 1;
+    //this shows your age in a pop up//
+    //tried to do also months and days but it doesn't work 100%//
+    //alert(`You are: ${calcYear} years, ${calcMonth} months and ${calcDay} days old`);//
+    //this pop up only shows years//
+    alert(`You are: ${calcYear} years old`);
+    //this is a way to show your age in the HTML//
+    document.getElementById('target').innerHTML = calcYear;
+};
