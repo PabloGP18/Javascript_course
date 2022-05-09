f07c4a2cbf87b5cee072c80c61d7328d447f0915
PabloGP18 <garciaplaza.pablo@gmail.com>
2022-05-09 14:24:12 +0200

    Exercise 5.1 done

--- js-complete-course/05-arrays/01-get-element/script.js
+++ js-complete-course/05-arrays/01-get-element/script.js
@@ -11,11 +11,11 @@
 
 (function() {
 
-    var fruits = [
+    const fruits = [
         "apple",
         "pear",
         "raspberry",
-        "tomatos",
+        "tomatoes",
         "kiwi",
         "banana",
         "orange",
